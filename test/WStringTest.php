9c1c9841c3f0757dab216f0de1ce2d2545eb669b
Ronmi Ren <ronmi.ren@gmail.com>
2015-11-06 11:12:28 +0800

    add test cases for isWide and stringWidth

--- test/WStringTest.php
+++ test/WStringTest.php
@@ -7,6 +7,41 @@ use Fruit\Convas\WString;
 
 class WStringTest extends PHPUnit_Framework_TestCase
 {
+    public function stringP()
+    {
+        return array(
+            array("a", 1),
+            array("0", 1),
+            array("=", 1),
+            array("爽", 2),
+            array("ㄝ", 2),
+            array("サ", 2),
+            array("럴", 2),
+            array("︶", 2),
+        );
+    }
+
+    /**
+     * @dataProvider stringP
+     */
+    public function testIsWide($char, $len)
+    {
+        $expect = $len == 2;
+        $actual = WString::isWide($char);
+
+        $this->assertEquals($expect, $actual, $char);
+    }
+
+    /**
+     * @dataProvider stringP
+     */
+    public function testStringWidth($char, $len)
+    {
+        $actual = WString::stringWidth($char);
+
+        $this->assertEquals($len, $actual, $char);
+    }
+
     public function testWordWrap()
     {
         $text = "On the day Adobe patched two of the Flash Player zero-day vulnerabilities uncovered following the Hacking Team breach, FireEye researchers noticed that, one of the flaws had been used in an attack aimed at organizations in Japan.\nItaly-based surveillance software company Hacking Team has suffered a data breach and hackers leaked a total of 400GB of data stolen from the spyware 看山山很小 maker’s systems. Researchers discovered exploits for several unpatched vulnerabilities after analyzing the Hacking Team leak, including Windows kernel, Microsoft Office, and Adobe Flash Player exploits.";
